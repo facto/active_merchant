e1b93a86afb108d94e208e9bd55dd7ee6d7e2b51
Joshua Rieken <joshua@joshuarieken.com>
2016-09-22 12:00:48 -0600

    Use two-digit year with Authorize.net

--- lib/active_merchant/billing/gateways/authorize_net.rb
+++ lib/active_merchant/billing/gateways/authorize_net.rb
@@ -380,7 +380,7 @@ module ActiveMerchant
           xml.payment do
             xml.creditCard do
               xml.cardNumber(truncate(credit_card.number, 16))
-              xml.expirationDate(format(credit_card.month, :two_digits) + '/' + format(credit_card.year, :four_digits))
+              xml.expirationDate(format(credit_card.month, :two_digits) + format(credit_card.year, :two_digits))
               if credit_card.valid_card_verification_value?(credit_card.verification_value, credit_card.brand)
                 xml.cardCode(credit_card.verification_value)
               end
